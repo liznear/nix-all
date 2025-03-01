085f4615fa299bbe175df08f8f8bc313f75e0a0d
nearsyh <nearsy.h@gmail.com>
2025-03-01 16:38:00 +0800

    Add dinit

--- home/zsh.nix
+++ home/zsh.nix
@@ -15,6 +15,11 @@
       z = "zellij";
       za = "zellij attach";
     };
+    initExtra = ''
+function dinit() {
+  nix flake init --template "https://flakehub.com/f/the-nix-way/dev-templates/*#$1"
+}
+'';
     plugins = [
       {
         name = "zsh-syntax-highlighting";
