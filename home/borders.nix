a92818c1d41b7ffc28378a5bf71a2351fdacd675
nearsyh <nearsy.h@gmail.com>
2025-05-17 09:23:12 +0800

    Fix borders configuration

--- home/borders.nix
+++ home/borders.nix
@@ -1,9 +1,12 @@
 { config, pkgs, lib, ... }:
 
 {
-    home.file.".config/borders/bordersrc".text = ''
+    home.file.".config/borders/bordersrc" = {
+        text = ''
 #!/usr/bin/env bash
 
 borders active_color=0xff65ba94 inactive_color=0xff494d64 width=10.0
 '';
+        executable = true;
+    };
 }
